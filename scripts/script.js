569b2032c5b443edb8c361fcf3149754fe46fad8
anissarashid <48539288+anissarashid@users.noreply.github.com>
2020-06-17 16:13:54 -0700

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,8 @@ $(document).ready(function() {
 		if (white & !black & !purple) {
 			$('body').css('background', '#323232');
 			$('p').css('color', '#f46b42');
-			$('.name').css('color', '#f46b42');
-			$('.contact, .title_thumb').css('color', '#f46b42');
-			$('.sidebar_item, .description_txt').css('color', '#f7f7f7');
+			$('.side, .contact, .contactt, .title_thumb, #newtitle, .dTitle, .boldwrd').css('color', '#f46b42');
+			$('.sidebar_item, .description_txt, .description_txt_1').css('color', '#f7f7f7');
 			black = true;
 			white = false;
 			purple = false;
@@ -16,22 +15,46 @@ $(document).ready(function() {
 		else if (black & !white & !purple) {
 			$('body').css('background', '#b0acc1');
 			$('p').css('color','#fce8c7');
-			$('.name').css('color','#fce8c7');
-			$('.contact').css('color','#fce8c7');
-			$('.title_thumb').css('color','#fce8c7');
-			$('sidebar_item, .description_txt').css('color', '#f7f7f7');
+			$('.side, .contact, .contactt, .title_thumb, #newtitle, .dTitle, .boldwrd').css('color','#fce8c7');
+			$('.sidebar_item, .description_txt, .description_txt_1').css('color', '#f7f7f7');
 			black = false;
 			purple = true;
 			white = false;
 		}
 
 		else {
-			$('body').css('background', '#fcf6e6');
-			$('p, .name, .contact, .title_thumb').css('color','black');
-			$('.sidebar_item, .description_txt').css('color', 'black');
+			$('body').css('background', '#f7f6ed');
+			$('p').css('color','black');
+			$('.side, .contact, .contactt, .title_thumb, .dTitle, .boldwrd').css('color','black');
+			$('.sidebar_item, .description_txt, .description_txt_1').css('color', 'black');
+			$('.txtover, #newtitle').css('color','#b51414');
 			black = false;
 			purple = false;
 			white = true;
 		}
 	});
 });
+
+
+$(window).scroll(function() {
+
+    //After scrolling 100px from the top...
+    if ( $(window).scrollTop() >= 300 ) {
+        $('.dTitle').css('color', '#dbd0d0');
+        
+
+    //Otherwise remove inline styles and thereby revert to original stying
+    } else {
+        $('.dTitle').attr('style', '');
+
+    }
+});
+
+
+
+
+
+
+
+
+
